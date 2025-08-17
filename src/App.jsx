c08745ec184fd7c4a0b6dd94cba1d98f943296f5
abdullahmahfouz <aboudmahfouz@gmail.com>
2025-08-17 12:41:47 -0400

    Initialize React + Vite project with UI components
    Set up a new React project using Vite, including configuration files, ESLint setup, and base directory structure. Added initial UI components, utility functions, and Tailwind CSS integration. Included basic documentation and project metadata.

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Children, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+const router = createBrowserRouter([
+  {
+    element:<AppLayout />,
+    Children:[
+      {
+        
+      }
+    ]
+  },
+ 
+]);
+
+function App() {
+   return (
+   
+  );
+}
+
+export default App
---
+++ src/components/app-layout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const app-layout = () => {
+  return (
+    <div>app-layout</div>
+  )
+}
+
+export default app-layout
